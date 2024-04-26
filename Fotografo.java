bd6dff81bba07334e74597336ff93b66ee66d927
ik012982i9 <a@a.a>
2024-04-26 10:15:37 +0200

    clase fotografo

---
+++ Fotografo.java
@@ -0,0 +1,29 @@
+public class Fotografo {
+    private int id;
+    private String nombre;
+    private boolean premiado;
+
+    // Constructor
+    public Fotografo(int id, String nombre, boolean premiado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.premiado = premiado;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean isPremiado() {
+        return premiado;
+    }
+
+    public String toString() {
+        return nombre;
+    }
+}
--- src/VisualizadorFotos.java
+++ src/VisualizadorFotos.java
@@ -1,4 +1,12 @@
 package src;
 
-public class VisualizadorFotos {
-}
+import javax.swing.*;
+
+public class VisualizadorFotos extends JFrame {
+    private JComboBox<Fotografo> comboBoxFotografos;
+    private JLabel labelFotografo;
+    private JXDatePicker datePicker;
+    private JLabel labelFecha;
+    private JList<Fotografia> listFotos;
+    private JLabel labelImagen;
+
